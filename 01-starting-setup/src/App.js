349e8b47156668550116ca7fa47a53cb37370214
Daniel Frystacky <danielfrystacky@gmail.com>
2021-05-31 12:44:21 -0400

    complete section 3

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,10 +1,28 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from "./components/Expenses";
 
 function App() {
+  const expense = [
+    {
+      title: "title1",
+      amount: 2.67,
+      date: new Date(2021, 4, 28),
+    },
+    {
+      title: "title2",
+      amount: 24.67,
+      date: new Date(2021, 5, 29),
+    },
+    {
+      title: "title3",
+      amount: 29.67,
+      date: new Date(2021, 6, 30),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <Expenses expense={expense} ></Expenses>
     </div>
   );
 }
